92c3c67944d4820ce1d69e18b072f37665806f4b
dishoomshubam <shubham.kharche@insightbridge.co>
2025-10-24 14:31:36 +0530

    langchain

--- langchain/main.py
+++ langchain/main.py
@@ -1,5 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv()
+
+
+
+
 def main():
     print("Hello from langchain!")
+    # print(os.environ.get("OPENAI"))
 
 
 if __name__ == "__main__":
