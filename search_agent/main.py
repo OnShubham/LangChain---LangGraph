968f81337c13c442bbd7580ee98780e564cd53a4
dishoomshubam <shubham.kharche@insightbridge.co>
2025-10-28 13:23:30 +0530

    search agent

--- search_agent/main.py
+++ search_agent/main.py
@@ -1,3 +1,14 @@
+from dotenv import load_dotenv
+load_dotenv()
+import time
+from langchainhub import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_openai import OpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+
 def main():
     print("Hello from search-agent!")
 
